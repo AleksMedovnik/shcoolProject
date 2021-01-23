2c7cc2e0f96c529fa81f84d67bbd7d0622c839e6
Aleksandr <medovnikaleksander@yandex.ua>
2021-01-23 23:37:46 +0300

    Added else to Calculator

--- scripts/index.js
+++ scripts/index.js
@@ -4,24 +4,26 @@ const calculate = () => {
     let result = prompt('Привет! Я калькулятор. Напишите выражение, которое хотите вычислить!', '');
     if (result == null) {
         alert('Ввод отменен');
-    };
+    } else{
+        result = result.replace(/,/g, ".");
 
-    result = result.replace(/,/g, ".");
-
-    try {
-        if (result == '') {
-            alert('Вы ничего не ввели!');
-        } else if (eval(result) == Infinity || eval(result) == -Infinity) {
-            alert('На нуль делить нельзя!');
-        } else {
-            alert(eval(result));
+        try {
+            if (result == '') {
+                alert('Вы ничего не ввели!');
+            } else if (eval(result) == Infinity || eval(result) == -Infinity) {
+                alert('На нуль делить нельзя!');
+            } else {
+                alert(eval(result));
+            }
+    
+        } catch (err) {
+    
+            alert(`Пожалуйста, введите корректное выражение! :)`); // (3) <--
+    
         }
-
-    } catch (err) {
-
-        alert(`Пожалуйста, введите корректное выражение! :)`); // (3) <--
-
     }
+
+    
 };
 
 const calculator = document.getElementById('calculator');
