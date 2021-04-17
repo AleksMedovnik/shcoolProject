9fbafeb23383cdf31a8979e04135b8e2edc69f15
Aleksandr <medovnikaleksander@yandex.ua>
2021-04-17 19:30:39 +0300

    Added cells

--- canvasSky/main.js
+++ canvasSky/main.js
@@ -3,11 +3,10 @@ const canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d'),
     w = canvas.width = document.documentElement.clientWidth,
     h = canvas.height = document.documentElement.clientHeight,
-    stars = [],
-    maxStars = 300;
+    cells = [],
+    maxCell = 300;
 
-// Thanks @jackrugile for the performance tip! https://codepen.io/jackrugile/pen/BjBGoM
-// Cache gradient
+
 const canvas2 = document.createElement('canvas'),
     ctx2 = canvas2.getContext('2d');
 canvas2.width = 100;
@@ -24,7 +23,7 @@ ctx2.beginPath();
 ctx2.arc(half, half, half, 0, Math.PI * 2);
 ctx2.fill();
 
-// End cache
+
 
 const random = (min = 0, max = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -35,18 +34,18 @@ const maxOrbit = (x, y) => {
     return diameter / 2;
 }
 
-const Star = function () {
+const Cell = function () {
 
     this.orbitRadius = random(maxOrbit(w, h));
     this.radius = random(60, this.orbitRadius) / 5; // 12
     this.orbitX = w / 2;
     this.orbitY = h / 2;
-    this.timePassed = random(0, maxStars);
+    this.timePassed = random(0, maxCell);
     this.speed = random(this.orbitRadius) / 700000; // 50000
-    stars.push(this);
+    cells.push(this);
 }
 
-Star.prototype.draw = function () {
+Cell.prototype.draw = function () {
     const x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
         y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY;
     ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
@@ -57,13 +56,13 @@ Star.prototype.draw = function () {
 const animation = () => {
     ctx.fillStyle = 'rgba(65, 13, 13, 0.863)';
     ctx.fillRect(0, 0, w, h);
-    stars.forEach(star => star.draw());
+    cells.forEach(cell => cell.draw());
 
     window.requestAnimationFrame(animation);
 }
 
-for (let i = 0; i < maxStars; i++) {
-    new Star();
+for (let i = 0; i < maxCell; i++) {
+    new Cell();
 }
 
 animation();
